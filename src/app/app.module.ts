eb6cc13defd9c913859f9c63d4a0e0a997523ec8
FJADL <filipdornbuschlind@gmail.com>
2022-01-27 21:35:14 +0100

    Shiba inu card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { MatDrawer, MatDrawerContainer, MatDrawerContent, MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { NavmenuComponent } from './navmenu/navmenu.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { CharacterCardComponent } from './character-card/character-card.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     NavmenuComponent,
     FooterComponent,
+    CharacterCardComponent,
   ],
   imports: [
+    MatCardModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
