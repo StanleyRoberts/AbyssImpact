02cabfad36262189ab1468803d9f4a948c375e65
StanleyRoberts <stanley.heronwood@gmail.com>
2022-01-28 18:48:47 +0000

    added kokomi as a display test

--- src/app/chars.ts
+++ src/app/chars.ts
@@ -9,7 +9,14 @@ export const CHARS: Char[] = [
         id: 1,
         name: "Amber",
         img: "assets/Character_Amber_Thumb.png",
-        desc: "A MainDPS who focuses on charged shots. Her low burst cooldown also allows her to support using \'Elegy of the End\"",
+        desc: "A MainDPS who focuses on charged shots. Her low burst cooldown also allows her to support using \'Elegy of the End\'",
         role: "MainDPS"
+    },
+    {
+        id: 2,
+        name: "Kokomi",
+        img: "assets/Character_Sangonomiya_Kokomi_Thumb.png",
+        desc: "A support who outputs an incredible amount of healing, hydro application and solid damage",
+        role: "Healer"
     }
 ];
