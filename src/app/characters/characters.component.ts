b123b1db3f108fe960fed4a3118cd5b42d8d6da1
StanleyRoberts <stanley.heronwood@gmail.com>
2022-02-01 20:42:26 +0000

    characters start loaded fix

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Char } from './../character-card/character-card.service';
 import { FormControl } from '@angular/forms';
-import { combineLatest, Observable, from } from 'rxjs';
+import { combineLatest, Observable, from, startWith } from 'rxjs';
 import { map } from 'rxjs/operators';
 import characters from '../../assets/data/character_data/characters.json'
 
@@ -24,15 +24,15 @@ export class CharactersComponent implements OnInit {
   constructor() { 
     this.chars$ = from(this.chars);
     this.filter = new FormControl();
-    this.filter$ = this.filter.valueChanges;
+    this.filter$ = this.filter.valueChanges.pipe(startWith([]));
      // i hate js: returns characters who have at least one role belonging to one filter
     this.filteredChars$ = combineLatest([this.chars$, this.filter$])
-                          .pipe(map(([, filterval])=>this.chars
-                          .filter(char=>filterval.filter(val=>char.roles
+                          .pipe(map(([, filterval])=> this.chars
+                          .filter(char=> !(filterval.filter(val=>char.roles
                           .map(function(x){return x.role;})
-                          .includes(val)).length>0)));
-    
+                          .includes(val)).length>0))));
   }
+
 
   ngOnInit(): void {
   }
