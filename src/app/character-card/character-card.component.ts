805db8bf160e78ca3d9798803e904b9f0eccbb34
FJADL <filipdornbuschlind@gmail.com>
2022-02-08 11:14:08 +0100

    Added comments to clarify

--- src/app/character-card/character-card.component.ts
+++ src/app/character-card/character-card.component.ts
@@ -11,13 +11,14 @@ import { FormGroup } from '@angular/forms';
 })
 export class CharacterCardComponent implements OnInit {
   addCharacter!: FormControl; //Creates a from that stores if the character is added
-  @Input('character') character!: Char;
-  cookieVal:any;
+  @Input('character') character!: Char; //Pulls in the Char class
 
   constructor() {}
 
   ngOnInit(): void {
+    //Grabs if you have a character from the local storage and if it's null returns false
     this.addCharacter = new FormControl((localStorage.getItem(this.character.id.toString()))==='true')
+    //Subscibes to the add button and updates if the user wants to add or remove a character from localstorage
     this.addCharacter.valueChanges.subscribe(x=>{localStorage.setItem(this.character.id.toString(), x)})
   }
 }
